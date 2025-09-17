1cf40291fc52f450820b3b4ac7cc8506d79f2d74
echoshade800 <echoshade800@gmail.com>
2025-09-17 18:32:19 +0800

    Fix Babel configuration to resolve React 'use' hook error

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function(api) {
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
     plugins: [
-      'expo-router/babel',
-      'react-native-web'
+      'expo-router/babel'
     ],
   };
 };
